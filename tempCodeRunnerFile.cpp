cd2135ba62474e85ea6302574519f98d27abab34
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-11-29 13:41:17 +0900

    ARC004A

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int N;
+    float x[100],y[100];
+    float dis;
+    float disMax;
+
+    cin >> N;
+
+    for(int i=0;i<N;i++){
+        cin >> x[i] >> y[i];
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            dis = sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]));
+        }
+        if(disMax<dis) disMax=dis;
+    }
+    cout << disMax;
+}
